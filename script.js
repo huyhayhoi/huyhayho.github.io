4c6b04333e1641fb28312e41c59c4aedb994cd24
huy0ham <huy0suy302@gmail.com>
2023-08-21 17:07:00 +0700

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //check key
-// function checkKey() {
-//     const key = 'key';
-//     let inputKey = document.getElementById("logkey").value;
+function checkKey() {
+    const key = 'key';
+    let inputKey = document.getElementById("logkey").value;
 
-//     if (inputKey === key) {
-//         document.querySelector(".card").classList.add("hidden");
-//         document.querySelector(".main").classList.remove("hidden");
-//     } else {
-//         alert('Sai rùi kìa. thử lại coai.');
-//     }
-// }
+    if (inputKey === key) {
+        document.querySelector(".open").classList.add("hidden");
+        document.querySelector(".main").classList.remove("hidden");
+    } else {
+        alert('Sai rùi kìa. thử lại coai.');
+    }
+}
 
 // Swiper1
 var swiper = new Swiper(".mySwiper", {
@@ -19,8 +19,19 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+//swiper2
+var swiper = new Swiper(".mySwiper2", {
+  effect: "cards",
+  grabCursor: true,
+});
+
+//swiper3
+
+
 //button
 let activeButton = null;
+const cnt1 = document.querySelector(".box1");
+const cnt2 = document.querySelector(".box2");
 
 function toggleColor(button) {
   if (activeButton !== null) {
@@ -33,8 +44,28 @@ function toggleColor(button) {
   } else {
     activeButton = null;
   }
+
+  // if (activeButton == null) {
+  //   document.querySelector(".box-content").style.display = "none";
+  // } else {
+  //   document.querySelector(".box-content").style.display = "block";
+  // }
+  if (document.querySelector(".btn1").classList.contains("active")) {
+    cnt1.classList.add("box-active");
+  } else {
+    cnt1.classList.remove("box-active");
+  }
+  if (document.querySelector(".btn2").classList.contains("active")) {
+    cnt2.classList.add("box-active");
+  } else {
+    cnt2.classList.remove("box-active");
+  }
 }
 
+// if (document.querySelector(".btn1").classList.contains("active")) {
+//     document.querySelector(".content").classList.add("box-active");
+//   }
+// )
 //accordion
 // const accordion = document.querySelectorAll(".box");
 // for(const i of accordion) {
